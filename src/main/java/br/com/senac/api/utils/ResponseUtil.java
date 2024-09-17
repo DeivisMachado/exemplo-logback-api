bdb0a13f96d11f32f4459c504761b15bd48c991b
DEIVIS MACHADO LINO <deivis.lino@alunos.sc.senac.br>
2024-09-17 20:17:42 -0300

    Alteração de teste

--- src/main/java/br/com/senac/api/utils/ResponseUtil.java
+++ src/main/java/br/com/senac/api/utils/ResponseUtil.java
@@ -8,6 +8,7 @@ public class ResponseUtil {
         Map<String, Object> response = new HashMap<>();
         response.put("messages", message);
 
+        System.out.println("Teste GIT");
         return response;
     }
 }
