d1624ee0067bc56336e5185bc036ec34e2a9e0c6
DEIVIS MACHADO LINO <deivis.lino@alunos.sc.senac.br>
2024-09-24 21:29:54 -0300

    Criação do Scheduler

---
+++ src/main/java/br/com/senac/api/scheduler/loading/ProdutosScheduler.java
@@ -0,0 +1,24 @@
+package br.com.senac.api.scheduler.loading;
+
+import br.com.senac.api.useCases.produtos.ProdutosService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutosScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProdutosScheduler.class);
+
+    @Autowired
+    private ProdutosService produtosService;
+
+    @Scheduled(fixedRate = 60000) // Executa a cada 1 minuto (60000 milissegundos)
+    public void carregarProdutosTrigger() {
+        logger.info("Iniciando carregamento de produtos...");
+        produtosService.carregarEExibirProdutos();
+        logger.info("Carregamento de produtos concluído.");
+    }
+}
