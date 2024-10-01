68d05abc6faaaf488cdb35022181a30c7cab4312
DEIVIS MACHADO LINO <deivis.lino@alunos.sc.senac.br>
2024-10-01 20:53:02 -0300

    criacao e configuracao ActiveMQ

--- src/main/java/br/com/senac/api/ExemploLogbackApiApplication.java
+++ src/main/java/br/com/senac/api/ExemploLogbackApiApplication.java
@@ -1,7 +1,14 @@
 package br.com.senac.api;
 
+import jakarta.jms.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,4 +19,25 @@ public class ExemploLogbackApiApplication {
 		SpringApplication.run(ExemploLogbackApiApplication.class, args);
 	}
 
+
+
+	@Bean
+	public JmsListenerContainerFactory<?> defaultFactory(
+			ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+		DefaultJmsListenerContainerFactory factory =
+				new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MappingJackson2MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter =
+				new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 }
